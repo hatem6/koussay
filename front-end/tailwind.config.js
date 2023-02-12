7a56a7763d4e861ce41b9388df1626df143cfd67
koussay-r <koussayr98@gmail.com>
2023-02-12 21:10:36 +0100

    getting back to this project and clearing some things up

--- front-end/tailwind.config.js
+++ front-end/tailwind.config.js
@@ -4,7 +4,12 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'quicksand': ['"Quicksand"', "sans-serif"],
+        'Montserrat': ['Montserrat', 'sans-serif']
+      },
+    },
   },
   plugins: [
     require('tailwind-scrollbar-hide')
