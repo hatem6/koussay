7489869862fafea1f6d52e0ed09f71b81275e7db
koussay-r <koussayr98@gmail.com>
2023-02-13 17:07:05 +0100

    update

--- back-end/MessagesSchema.js
+++ back-end/MessagesSchema.js
@@ -3,7 +3,6 @@ import mongoose from "mongoose";
 
 const messagesSchema=mongoose.Schema({
     nameCollec1:String,
-    nameCollec2:String,
     name:String,
     message:String
 })
--- back-end/Routes.js
+++ back-end/Routes.js
@@ -2,11 +2,14 @@ import express  from "express";
 import mongoose from "mongoose";
 import {collecModel,collectionUsers} from "./server.js"
 import Pusher from "pusher";
-import messagesSchema from "./MessagesSchema.js";
 export const routes=express.Router()
-routes.post('/sendMesssage',(req,res)=>{
 
-
+routes.get('/sendMesssage',(req,res)=>{
+  const zero="0"
+   collecModel.find({},(err,data)=>{
+    if(err) res.status(500).send(err)
+    else res.status(200).send(data[0].ArrayMessages[0])
+   })
     const db = mongoose.connection;
     db.on('error', console.error.bind(console, 'connection error:'));
     db.once('open', function() {
